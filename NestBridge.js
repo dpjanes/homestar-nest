d48595c673d5eefc2a54e7b7e0d259333b2a9d2f
David Janes <davidjanes@davidjanes.com>
2016-02-06 10:27:18 -0500

    fixed up 'push' validation

--- NestBridge.js
+++ NestBridge.js
@@ -175,7 +175,7 @@ NestBridge.prototype.push = function (pushd, done) {
         return;
     }
 
-    self._validate_push(pushd);
+    self._validate_push(pushd, done);
 
     logger.info({
         method: "push",
